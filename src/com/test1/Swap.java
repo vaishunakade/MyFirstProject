b5e91b1d6928386d2a83a2b6de608f2514df8415
amitk <amitk@DESKTOP-UCUI60M>
2024-03-20 15:18:44 +0530

    Swap

---
+++ src/com/test1/Swap.java
@@ -0,0 +1,14 @@
+package com.test1;
+
+public class Swap {
+
+	public static void main(String[] args) {
+		int x=10;
+		int y=20;
+		int temp;
+		temp = y;
+		y=x;
+		x = temp;
+		System.out.println("x=" + x + " y="+y);	}
+
+}
