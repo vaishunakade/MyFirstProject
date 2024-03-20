1c0a9234705a059f777a011015a03a317f5a23da
amitk <amitk@192.168.1.2>
2024-03-20 22:09:21 +0530

    Pattern_number

---
+++ src/com/test1/Number_pattern.java
@@ -0,0 +1,18 @@
+package com.test1;
+
+public class Number_pattern {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=1;j<=5;j++)
+			{
+				System.out.print("A");
+			}
+			
+			System.out.println(" ");
+		}
+	}
+}
+
+
